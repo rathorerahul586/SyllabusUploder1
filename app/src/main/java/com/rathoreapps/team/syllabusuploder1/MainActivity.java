9439000669790456c6e03c5b0b0c3bc50e769bc0
rathorerahul586 <32606526+rathorerahul586@users.noreply.github.com>
2019-07-27 11:37:17 +0530

    Congratulation Complete Working well No issue no error no warning as per my knowledge All logs are also removed
    Code Rearranged

--- app/src/main/java/com/rathoreapps/team/syllabusuploder1/MainActivity.java
+++ app/src/main/java/com/rathoreapps/team/syllabusuploder1/MainActivity.java
@@ -1,9 +1,5 @@
 package com.rathoreapps.team.syllabusuploder1;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
@@ -20,6 +16,10 @@ import android.widget.RelativeLayout;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -33,15 +33,16 @@ public class MainActivity extends AppCompatActivity {
     private FirebaseDatabase database;
     private AutoCompleteTextView subjectET;
     private EditText branchNameET, noOfSemesterET, chapterNameET, contentET, unitNoET;
-    private Button addBranchBtn, addSubjectBtn, addSyllabusBtn, hideBranch,hideSubject,hideSyllabus;
-    private String branchName, noOfSemester, subjectName, chapterName,content,unitNo;
+    private Button addBranchBtn, addSubjectBtn, addSyllabusBtn, hideBranch, hideSubject, hideSyllabus;
+    private String branchName, noOfSemester, subjectName, chapterName, content, unitNo;
     private Spinner branchListSpinner, semesterListSpinner, subjectListSpinner;
-    private String[] branchListForSpinner, semesterListForSpinner,subjectListForSpinner;
+    private String[] branchListForSpinner, semesterListForSpinner, subjectListForSpinner;
     private ArrayAdapter<String> branchSpinnerAdapter, semesterListAdepter, subjectListAdapter;
     private ProgressBar branchSpinnerProgressBar, semesterListSpinnerProgressBar, subjectListSpinnerProgressBar;
     private RelativeLayout branchSpinnerLayout, semesterListSpinnerLayout, subjectListSpinnerLayout;
     private LinearLayout syllabusLayout;
     private String selectedBranch, selectedSemester;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
         setAddSyllabus();
     }
 
-    private void initViews(){
+    private void initViews() {
         context = getBaseContext();
         branchSpinnerLayout = findViewById(R.id.branch_spinner_layout);
         semesterListSpinnerLayout = findViewById(R.id.semester_list_spinner_layout);
@@ -76,6 +77,7 @@ public class MainActivity extends AppCompatActivity {
                 selectedBranch = adapterView.getSelectedItem().toString();
                 readSemesterList(selectedBranch);
             }
+
             @Override
             public void onNothingSelected(AdapterView<?> adapterView) {
                 Toast.makeText(context, "Please Select Any Branch Name form Dropdown Menu", Toast.LENGTH_SHORT).show();
@@ -101,28 +103,27 @@ public class MainActivity extends AppCompatActivity {
         noOfSemesterET = findViewById(R.id.no_of_semester);
     }
 
-    private void setAddBranch (){
+    private void setAddBranch() {
         addBranchBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (branchNameET.getVisibility()==View.GONE) {
+                if (branchNameET.getVisibility() == View.GONE) {
                     branchNameET.setVisibility(View.VISIBLE);
                     noOfSemesterET.setVisibility(View.VISIBLE);
                     hideBranch.setVisibility(View.VISIBLE);
-                }else{
+                } else {
                     branchName = branchNameET.getText().toString().trim();
                     noOfSemester = noOfSemesterET.getText().toString().trim();
-                    if (branchName.isEmpty() || noOfSemester.isEmpty()){
+                    if (branchName.isEmpty() || noOfSemester.isEmpty()) {
                         Toast.makeText(MainActivity.this, "Please Enter Branch Name and No. of semesters", Toast.LENGTH_SHORT).show();
-                    }else if (branchName.contains(".")||
-                            branchName.contains("$")||
-                            branchName.contains("[")||
-                            branchName.contains("]")||
-                            branchName.contains("#")||
-                            branchName.contains("/")){
+                    } else if (branchName.contains(".") ||
+                            branchName.contains("$") ||
+                            branchName.contains("[") ||
+                            branchName.contains("]") ||
+                            branchName.contains("#") ||
+                            branchName.contains("/")) {
                         Toast.makeText(MainActivity.this, "Branch Name Must Not Contain any of \n . , # , [ , ] , # , / ", Toast.LENGTH_SHORT).show();
-                    }
-                    else {
+                    } else {
                         int getNoOfSemester = Integer.valueOf(noOfSemester);
                         addNewBranchToDatabase(branchName, getNoOfSemester);
                         Toast.makeText(MainActivity.this, branchName + " added to Database", Toast.LENGTH_SHORT).show();
@@ -137,7 +138,7 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private void setAddSubjectBtn(){
+    private void setAddSubjectBtn() {
         addSubjectBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -147,20 +148,19 @@ public class MainActivity extends AppCompatActivity {
                     subjectET.setVisibility(View.VISIBLE);
                     hideSubject.setVisibility(View.VISIBLE);
                     readBranchList();
-                }else{
+                } else {
                     subjectName = subjectET.getText().toString().trim();
-                    if (subjectName.isEmpty()){
+                    if (subjectName.isEmpty()) {
                         Toast.makeText(MainActivity.this, "Please Enter Subject Name", Toast.LENGTH_SHORT).show();
-                    }
-                    else {
-                        final AlertDialog.Builder builder= new AlertDialog.Builder(MainActivity.this);
+                    } else {
+                        final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
 
                         builder.setTitle("Check Carefully")
-                                .setMessage("Branch Name: "+selectedBranch+"\nSemester: "+selectedSemester+"\nSubject Name: "+subjectName);
+                                .setMessage("Branch Name: " + selectedBranch + "\nSemester: " + selectedSemester + "\nSubject Name: " + subjectName);
                         builder.setPositiveButton("Add", new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialogInterface, int i) {
-                                addNewSubjectToDatabase(selectedBranch,selectedSemester,subjectName);
+                                addNewSubjectToDatabase(selectedBranch, selectedSemester, subjectName);
                                 dialogInterface.dismiss();
                                 subjectET.getText().clear();
                             }
@@ -179,7 +179,7 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private void setAddSyllabus(){
+    private void setAddSyllabus() {
         addSyllabusBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -190,49 +190,54 @@ public class MainActivity extends AppCompatActivity {
                     semesterListSpinnerLayout.setVisibility(View.VISIBLE);
                     syllabusLayout.setVisibility(View.VISIBLE);
                     readBranchList();
-                }else{
+                } else {
                     branchName = branchListSpinner.getSelectedItem().toString();
-                    if (subjectListAdapter.getCount() == 0){
+                    if (subjectListAdapter.getCount() == 0) {
                         Toast.makeText(context, "Their is no subject in Selected branch and semester. Please add Subject first", Toast.LENGTH_SHORT).show();
-                    }else {subjectName = subjectListSpinner.getSelectedItem().toString();
+                    } else {
+                        subjectName = subjectListSpinner.getSelectedItem().toString();
                         chapterName = chapterNameET.getText().toString().trim();
                         content = contentET.getText().toString().trim();
                         unitNo = unitNoET.getText().toString().trim();
-                        if (subjectName.isEmpty()) Toast.makeText(context, "Please Select any Subject", Toast.LENGTH_SHORT).show();
-                        else if (unitNo.isEmpty()) Toast.makeText(context, "Enter Unit No", Toast.LENGTH_SHORT).show();
-                        else if (chapterName.isEmpty()) Toast.makeText(context, "Enter Chapter Name", Toast.LENGTH_SHORT).show();
-                        else if (content.isEmpty()) Toast.makeText(context, "Enter Content", Toast.LENGTH_SHORT).show();
-                        else if (chapterName.contains(".")||
-                                chapterName.contains("$")||
-                                chapterName.contains("[")||
-                                chapterName.contains("]")||
-                                chapterName.contains("#")||
-                                chapterName.contains("/")){
+                        if (subjectName.isEmpty())
+                            Toast.makeText(context, "Please Select any Subject", Toast.LENGTH_SHORT).show();
+                        else if (unitNo.isEmpty())
+                            Toast.makeText(context, "Enter Unit No", Toast.LENGTH_SHORT).show();
+                        else if (chapterName.isEmpty())
+                            Toast.makeText(context, "Enter Chapter Name", Toast.LENGTH_SHORT).show();
+                        else if (content.isEmpty())
+                            Toast.makeText(context, "Enter Content", Toast.LENGTH_SHORT).show();
+                        else if (chapterName.contains(".") ||
+                                chapterName.contains("$") ||
+                                chapterName.contains("[") ||
+                                chapterName.contains("]") ||
+                                chapterName.contains("#") ||
+                                chapterName.contains("/")) {
                             Toast.makeText(MainActivity.this, "Chapter Name Must Not Contain any of \n . , # , [ , ] , # , / ", Toast.LENGTH_SHORT).show();
-                        }
-                        else {
-                            addNewSyllabusToDatabase(branchName,selectedSemester, subjectName,unitNo,chapterName);
+                        } else {
+                            addNewSyllabusToDatabase(branchName, selectedSemester, subjectName, unitNo, chapterName);
                             branchNameET.getText().clear();
                             subjectET.getText().clear();
                             chapterNameET.getText().clear();
                             contentET.getText().clear();
                             unitNoET.getText().clear();
-                        }}
+                        }
+                    }
 
                 }
             }
         });
     }
 
-    private void addNewBranchToDatabase(String branch, int noOfSemester){
+    private void addNewBranchToDatabase(String branch, int noOfSemester) {
         database = FirebaseDatabase.getInstance();
         myRef = database.getReference().child("Branches").child(branch);
-        for (int i = 1; i<= noOfSemester; i++){
-            myRef.child("Semester "+i).setValue("Semester "+i);
+        for (int i = 1; i <= noOfSemester; i++) {
+            myRef.child("Semester " + i).setValue("Semester " + i);
         }
     }
 
-    private void addNewSubjectToDatabase(String branch, String semester, String subject){
+    private void addNewSubjectToDatabase(String branch, String semester, String subject) {
         database = FirebaseDatabase.getInstance();
         myRef = database.getReference().child("Branches").child(branch).child(semester).child(subject);
         myRef.setValue(subject);
@@ -240,13 +245,13 @@ public class MainActivity extends AppCompatActivity {
 
     private void addNewSyllabusToDatabase(String branch, String semester, String subject, String unit, String chapterName) {
         database = FirebaseDatabase.getInstance();
-        myRef =  database.getReference().child("Branches").child(branch).child(semester).child(subject).child("Unit: "+unit+" "+chapterName);
+        myRef = database.getReference().child("Branches").child(branch).child(semester).child(subject).child("Unit: " + unit + " " + chapterName);
         myRef.setValue(content);
 
 
     }
 
-    private void readBranchList(){
+    private void readBranchList() {
         branchSpinnerProgressBar.setVisibility(View.VISIBLE);
         database = FirebaseDatabase.getInstance();
         myRef = database.getReference().child("Branches");
@@ -256,7 +261,7 @@ public class MainActivity extends AppCompatActivity {
                 int i = 0;
                 int childCount = (int) dataSnapshot.getChildrenCount();
                 branchListForSpinner = new String[childCount];
-                for (DataSnapshot ds: dataSnapshot.getChildren()){
+                for (DataSnapshot ds : dataSnapshot.getChildren()) {
                     String name = ds.getKey();
                     branchListForSpinner[i] = name;
                     i++;
@@ -277,7 +282,7 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private void readSemesterList(String branchName){
+    private void readSemesterList(String branchName) {
         semesterListSpinnerProgressBar.setVisibility(View.VISIBLE);
         database = FirebaseDatabase.getInstance();
         myRef = database.getReference().child("Branches").child(branchName);
@@ -287,7 +292,7 @@ public class MainActivity extends AppCompatActivity {
                 int i = 0;
                 int semCount = (int) dataSnapshot.getChildrenCount();
                 semesterListForSpinner = new String[semCount];
-                for (DataSnapshot ds : dataSnapshot.getChildren()){
+                for (DataSnapshot ds : dataSnapshot.getChildren()) {
                     String name = ds.getKey();
                     semesterListForSpinner[i] = name;
                     i++;
@@ -299,6 +304,7 @@ public class MainActivity extends AppCompatActivity {
                 semesterListSpinnerProgressBar.setVisibility(View.GONE);
 
             }
+
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
 
@@ -306,7 +312,7 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private void readSubjectList(String branch, String semester){
+    private void readSubjectList(String branch, String semester) {
         subjectListSpinnerLayout.setVisibility(View.VISIBLE);
         subjectListSpinnerProgressBar.setVisibility(View.VISIBLE);
         database = FirebaseDatabase.getInstance();
@@ -315,14 +321,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 int i = 0;
-                int subjectCount = (int)dataSnapshot.getChildrenCount();
+                int subjectCount = (int) dataSnapshot.getChildrenCount();
                 subjectListForSpinner = new String[subjectCount];
-                for (DataSnapshot ds : dataSnapshot.getChildren()){
+                for (DataSnapshot ds : dataSnapshot.getChildren()) {
                     String name = ds.getKey();
                     subjectListForSpinner[i] = name;
                     i++;
                 }
-                subjectListAdapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_spinner_dropdown_item,subjectListForSpinner);
+                subjectListAdapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_spinner_dropdown_item, subjectListForSpinner);
                 subjectListAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
                 subjectListSpinner.setVisibility(View.VISIBLE);
                 subjectListSpinner.setAdapter(subjectListAdapter);
@@ -336,7 +342,7 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public void setHideBranch(View view){
+    public void setHideBranch(View view) {
         branchSpinnerLayout.setVisibility(View.GONE);
         branchNameET.getText().clear();
         branchNameET.setVisibility(View.GONE);
@@ -345,7 +351,7 @@ public class MainActivity extends AppCompatActivity {
         hideBranch.setVisibility(View.GONE);
     }
 
-    public void setHideSubject(View view){
+    public void setHideSubject(View view) {
         branchSpinnerLayout.setVisibility(View.GONE);
         semesterListSpinnerLayout.setVisibility(View.GONE);
         subjectET.getText().clear();
@@ -353,7 +359,7 @@ public class MainActivity extends AppCompatActivity {
         hideSubject.setVisibility(View.GONE);
     }
 
-    public void setHideSyllabus(View view){
+    public void setHideSyllabus(View view) {
         branchSpinnerLayout.setVisibility(View.GONE);
         semesterListSpinnerLayout.setVisibility(View.GONE);
         hideSyllabus.setVisibility(View.GONE);
